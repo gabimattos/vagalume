4c56dfb87f7635f9efa0bdf87cd5e463e7459ee4
Gabriela Mattos <gabimattos83@hotmail.com>
2021-02-15 00:55:00 -0300

    + criando crud de musicas

--- backend/vagalume/app/Models/Music.php
+++ backend/vagalume/app/Models/Music.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Music extends Model
 {
     use HasFactory;
+
+    public function saveMusic (Request $request){
+        $this->title = $request->title;
+        $this->url = $request->url;
+        $this->save();
+    }
+
+    public function updateMusic(Request $request, $id){
+        if($request->title){
+            $this->title = $request->title;
+        }
+        $this->save();
+    }
 }
