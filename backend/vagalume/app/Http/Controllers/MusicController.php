e7e8fdf1a77b769aa2a4aa198d7429de59298b27
Gabriela Mattos <gabimattos83@hotmail.com>
2021-02-15 09:27:27 -0300

    + criando crud na controller

--- backend/vagalume/app/Http/Controllers/MusicController.php
+++ backend/vagalume/app/Http/Controllers/MusicController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Music;
 
 class MusicController extends Controller
 {
-    //
+    public function create (Request $request){
+        $music = new Music;
+        $music -> title = $request->title;
+        $music -> url = $request->url;
+        $music -> save();
+        return response()->json(['music' => $music], 200);
+    }
+
+    public function index(){
+        $musics = Music::all();
+        return response()->json(['musics'=>$musics], 200);
+    }
+
+    public function show ($id){
+        $music = Music::find($id);
+        return response()->json(['music' => $music], 200);
+    }
+
+    public function update (Request $request, $id){
+
+        $music = Music::find($id);
+        if($request->title){
+            $music->title = $request->title;
+        }
+        if($request->url){
+            $music->url = $request->url;
+        }
+        $music->save();
+        return response()->json(['music' => $music], 200);
+    }
+
+    public function delete($id){
+        Music::destroy($id);
+
+        return response()->json(['musica deletada'], 200);
+    }
+
 }
