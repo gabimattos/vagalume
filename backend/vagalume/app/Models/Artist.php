3825ddd79a030e855fb5759fb970c0a4407cc191
Gabriela Mattos <gabimattos83@hotmail.com>
2021-02-14 23:41:02 -0300

    + criando crud de artistas

--- backend/vagalume/app/Models/Artist.php
+++ backend/vagalume/app/Models/Artist.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Artist extends Model
 {
     use HasFactory;
+
+    public function saveArtist (Request $request){
+        $this->name = $request->name;
+        $this->save();
+    }
+
+    public function updateArtist(Request $request, $id){
+        if($request->name){
+            $this->name = $request->name;
+        }
+        $this->save();
+    }
 }
