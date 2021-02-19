a74dbf650b58a1596b04c08272cbcac28520fa9a
Gabriela Mattos <gabimattos83@hotmail.com>
2021-02-19 19:42:39 -0300

    # alterando código da função show para requisição da api

--- backend/vagalume/app/Http/Controllers/ArtistController.php
+++ backend/vagalume/app/Http/Controllers/ArtistController.php
@@ -21,9 +21,21 @@ class ArtistController extends Controller
         return response()->json(['artists'=>$artists], 200);
     }
 
-    public function show ($id){
-        $artist = Artist::find($id);
+    public function show ($name){
+        $artist = Artist::find($name);
+
+        $client = new Client([
+            'base_uri' => 'https://api.vagalume.com.br'
+        ]);
+
+            $api_key=  env ('KEY');
+
+        $response = $client->request('GET', "search.art?apikey={$api_key}&q={$name}");
+        
+        $results = json_decode($response->getBody()->getContents());
+
         return response()->json(['artist' => $artist], 200);
+        return response()->json($results);
     }
 
     public function update (Request $request, $id){
